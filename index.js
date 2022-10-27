21c2ec44782a73911526928d3377315342b23882
zubayer <zubayer3570@gmail.com>
2022-10-27 16:27:23 +0600

    done

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const cors = require("cors")
+const nodemailer = require("nodemailer")
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+// transporter
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: "zubayer3570@gmail.com",
+        pass: "fpwuuncbzrobohvn"
+    }
+})
+app.post("/sendmail", (req, res) => {
+    const data = req.body
+    transporter.sendMail({
+        from: data.email,
+        to: "zubayer3570@gmail.com",
+        subject: "New Client",
+        text: data.text
+    })
+    transporter.close()
+    console.log("success")
+    res.send({message: "mail sent successfully"})
+})
+app.listen(5000)
